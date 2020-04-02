44c5e61e05cbf1dc8339eb1a8e96a4e8ba8bf4e4
Shahar Evron <shahar.evron@datopian.com>
2020-04-02 16:26:49 +0300

    Fix entity binding issue by importing entity modules to package namespace

--- ckanext/authz_service/authz_binding/__init__.py
+++ ckanext/authz_service/authz_binding/__init__.py
@@ -1,3 +1,5 @@
+# We need to import the following modules to bind authorization functions
+from . import dataset, organization, resource  # noqa: F401
 from .common import authzzie
 
 __all__ = ['authzzie']
