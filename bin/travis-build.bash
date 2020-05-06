0f593ce5794154c2fae57bbb42df7e6261b8397e
Shahar Evron <shahar.evron@datopian.com>
2020-05-06 17:59:49 +0300

    Align Makefile and CI scripts with improvements made for ckanext-external-storage

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -3,48 +3,12 @@ set -e
 
 echo "This is travis-build.bash..."
 echo "Targetting CKAN $CKANVERSION on Python $TRAVIS_PYTHON_VERSION"
-if [ $CKANVERSION == 'master' ]
-then
-    export CKAN_MINOR_VERSION=100
-else
-    export CKAN_MINOR_VERSION=${CKANVERSION##*.}
-fi
-
-export PYTHON_MAJOR_VERSION=${TRAVIS_PYTHON_VERSION%.*}
-
 
 echo "Installing the packages that CKAN requires..."
 sudo apt-get update -qq
 sudo apt-get install solr-jetty
 
-echo "Installing CKAN and its Python dependencies..."
-git clone https://github.com/ckan/ckan
-cd ckan
-if [ $CKANVERSION == 'master' ]
-then
-    echo "CKAN version: master"
-else
-    CKAN_TAG=$(git tag | grep ^ckan-$CKANVERSION | sort --version-sort | tail -n 1)
-    git checkout $CKAN_TAG
-    echo "CKAN version: ${CKAN_TAG#ckan-}"
-fi
-
-if [ -e requirement-setuptools.txt ]
-then
-    pip install -r requirement-setuptools.txt
-fi
-
-python setup.py develop
-
-if [[ "$PYTHON_MAJOR_VERSION" == "2" && -e requirements-py2.txt ]]
-then
-    pip install -r requirements-py2.txt
-else
-    pip install -r requirements.txt
-fi
-
-pip install -r dev-requirements.txt
-cd -
+make ckan-install CKAN_VERSION=$CKANVERSION
 
 echo "Setting up Solr..."
 # solr is multicore for tests on ckan master now, but it's easier to run tests
@@ -56,12 +20,9 @@ sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
 sudo service jetty restart
 
 echo "Creating the PostgreSQL user and database..."
-sudo -u postgres psql -c "CREATE USER ckan_default WITH PASSWORD 'pass';"
-sudo -u postgres psql -c "CREATE USER datastore_default WITH PASSWORD 'pass';"
-sudo -u postgres psql -c 'CREATE DATABASE ckan_test WITH OWNER ckan_default;'
-sudo -u postgres psql -c 'CREATE DATABASE datastore_test WITH OWNER ckan_default;'
-
-echo "Setting the environment for ckanext-authz-service testing..."
-make develop CKAN_PATH=./ckan
+sudo -u postgres psql -c "CREATE USER ckan WITH PASSWORD 'ckan';"
+sudo -u postgres psql -c "CREATE USER datastore_ro WITH PASSWORD 'datastore_ro';"
+sudo -u postgres psql -c 'CREATE DATABASE ckan_test WITH OWNER ckan;'
+sudo -u postgres psql -c 'CREATE DATABASE datastore_test WITH OWNER ckan;'
 
 echo "travis-build.bash is done."
